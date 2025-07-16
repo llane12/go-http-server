db63fb31ec3e5efc050a82a5ba0a6328df74c961
llane12 <lukelane1@gmail.com>
2025-07-17 08:48:37 +0900

    Add middleware to track the number of fileserver requests

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", healthz)
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -23,8 +35,27 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func healthz(resp http.ResponseWriter, req *http.Request) {
+func handlerReadiness(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	resp.WriteHeader(http.StatusOK)
 	io.WriteString(resp, "OK")
+}
+
+func (cfg *apiConfig) handlerMetrics(resp http.ResponseWriter, req *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	resp.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	resp.WriteHeader(http.StatusOK)
+	io.WriteString(resp, fmt.Sprintf("Hits: %d", hits))
+}
+
+func (cfg *apiConfig) handlerReset(resp http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	resp.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
